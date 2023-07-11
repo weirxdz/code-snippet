6d3552ef6b2e56d0d94a69a850ba25eae87d4682
ADMIN <hany7u@gmail.com>
2023-07-11 22:09:31 +0800

    借款余额修正

---
+++ Oracle/借款单余额错误修正.sql
@@ -0,0 +1,10 @@
+/**/
+select er_busitem.pk_busitem,er_jkzb.djbh,er_jkzb.bbye,er_busitem.bbye,er_busitem.ybye,er_busitem.yjye,er_busitem.cjkbbje,er_jkzb.* from er_busitem er_busitem 
+left join er_jkzb er_jkzb on er_jkzb.pk_jkbx=er_busitem.pk_jkbx where er_jkzb.djbh='263X2017120900001161'
+;
+/*update er_busitem set ybye = '5880.15',bbye = '5880.15',yjye = '5880.15' where pk_busitem = '1001B3100000000NCVSS';
+commit;
+*/
+
+;
+
