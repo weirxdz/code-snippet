a1869cc8cf696a9debd6b2e025bdbbad682db7f2
ADMIN <hany7u@gmail.com>
2023-07-19 21:19:50 +0800

    扫码获取网址后截取最右8位，赋值给原控件

--- 帆软/控制控件是否可用的JavaScript脚本.js
+++ 帆软/控制控件是否可用的JavaScript脚本.js
@@ -20,4 +20,12 @@ if(ch == "酒罐"){
 	}else {
 	_g().getWidgetByCell("D36").setEnable(true);
 	}
-	
+
+//获取当前文本控件值
+//如果当前值的长度超过8位，截取最右8位
+//用于扫码填写酒坛号的处理，文本控件上设置编辑后事件
+//相关单元格的单元格属性→其他→插入行策略 选择“原值”
+var str = this.getValue();
+if(str.length > 8){
+	this.setValue(str.slice(-8),true);
+	}
