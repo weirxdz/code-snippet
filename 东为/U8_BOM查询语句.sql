c795838f3d5e1171d84b693404a945a36cf7c502
ADMIN <151980581+weirxdz@users.noreply.github.com>
2023-12-08 16:58:18 +0800

    Update U8_BOM查询语句.sql

--- 东为/U8_BOM查询语句.sql
+++ 东为/U8_BOM查询语句.sql
@@ -29,9 +29,7 @@ SELECT * FROM bas_part bp
 
 
 
---select  ccode as 科目编码,  cauth as 功能名称,  cuser as 用户名,  cmachine as 机器名  from GL_mccontrol;
---
---delete from GL_mccontrol
+
 
 SELECT * FROM fitemss00 f ;
 SELECT * FROM UFSUB_fitemss00 uf 
