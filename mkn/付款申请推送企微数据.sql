c9296d270efa7325a7b236c8f596628d281aaa88
ADMIN <151980581+weirxdz@users.noreply.github.com>
2024-01-02 22:17:25 +0800

    Update 付款申请推送企微数据.sql

--- mkn/付款申请推送企微数据.sql
+++ mkn/付款申请推送企微数据.sql
@@ -8,19 +8,26 @@
 -- U8单据附件表
 SELECT t.VoucherTypeID ,t.VoucherID ,t.FileID ,t.FileName ,t.FileContent ,t.Memo  FROM VoucherAccessories t
 ;
--- 以下为U8的采购请购单查询语句
-SELECT h.ID  ,h.cCode  ,h.dDate  ,d.cDepName AS deptname,p.cPersonName  as clerkname,isnull(P.WXID,'XiaoTong') AS WXID,h.cMemo ,h.wxOaState,h.cMaker ,h.cVerifier ,h.cAuditDate ,h.cAuditTime ,h.iverifystateex ,b.cInvCode ,i.cInvName,i.cInvStd  ,b.fQuantity  ,b.dRequirDate  AS 需求日期
-FROM PU_AppVouch h
-INNER JOIN PU_AppVouchs b ON h.ID = b.ID 
+-- 以下为U8的付款申请单查询语句
+SELECT h.pID  ,h.cVouchID ,h.dVouchDate   ,d.cDepName AS deptname,p.cPersonName  as clerkname,hp.cPsnPostAddr AS 业务员账号,UU.cUserEmail AS 制单人账号 ,isnull(P.WXID,'XiaoTong') AS WXID,h.cDigest  AS 备注,h.wxOaState,h.cOperator  ,h.cCheckMan  ,h.dverifydate  ,h.dverifysystime  ,b.cInvCode ,i.cInvName,i.cInvStd  ,b.iApplyAmt  表体付款金额  
+FROM AP_ApplyPayVouch h
+INNER JOIN AP_ApplyPayVouchs  b ON h.pID  = b.pID 
 LEFT JOIN Department d ON h.cDepCode = d.cDepCode 
-LEFT JOIN Person p ON h.cPersonCode = p.cPersonCode 
-INNER JOIN Inventory i ON b.cInvCode = i.cInvCode 
+LEFT JOIN Person p ON h.cPerson = p.cPersonCode
+LEFT JOIN hr_hi_person hp ON h.cPerson = hp.cPsn_Num 
+LEFT JOIN UA_User uu ON H.cOperator = UU.cUser_Name 
+LEFT JOIN Inventory i ON b.cInvCode = i.cInvCode 
 WHERE 1=1-- h.iStatus = 0 
-	-- AND convert(varchar(10),h.dDate,120) >= '2023-05-20' -- 开始同步日期，接口只处理此日期之后的记录
+	-- AND convert(varchar(10),h.dVouchDate,120) >= '2023-05-20' -- 开始同步日期，接口只处理此日期之后的记录
 	AND (h.wxOaState IS NULL OR h.wxOaState = 0)
 	
-	
+;
+SELECT * FROM AP_ApplyPayVouch T WHERE T.cVouchID = '0000000039'
 	
 -- 对U8数据库表需要增加的列
-ALTER TABLE UFDATA_999_2014.dbo.PU_AppVouch  ADD wxOaState int NULL;
-EXEC UFDATA_999_2014.sys.sp_addextendedproperty 'MS_Description', N'推送微信状态', 'schema', N'dbo', 'table', N'PO_Pomain', 'column', N'wxOaState';
+ALTER TABLE UFDATA_999_2014.dbo.AP_ApplyPayVouch  ADD wxOaState int NULL;
+EXEC UFDATA_999_2014.sys.sp_addextendedproperty 'MS_Description', N'推送微信状态', 'schema', N'dbo', 'table', N'AP_ApplyPayVouch', 'column', N'wxOaState';
+
+;
+SELECT * FROM hr_hi_person hhp ;
+SELECT * FROM UA_User t  
