9f6641d6259c834a418658e143ecf51221addf3f
ADMIN <151980581+weirxdz@users.noreply.github.com>
2024-03-25 01:52:05 +0800

    Update 付款申请单上来源单据的相关字段.sql

--- mkn/付款申请单上来源单据的相关字段.sql
+++ mkn/付款申请单上来源单据的相关字段.sql
@@ -3,9 +3,16 @@
 SELECT t.PID ,t.cVouchID  ,t.dvouchDate ,t.cOperator ,t.dcreatesystime , t.cCheckMan ,t.dverifydate  ,t.dverifysystime  ,t.wxOaState ,t.wxOaSpNo ,t.wxOaDate, t.cSource ,t.cSource ,t.*--,t.iBVid ,t.iPOsID 
 FROM AP_ApplyPayVouch t ;
 
-SELECT h.cVouchID ,h.cSource ,b.cSource ,b.PID,b.cCoVouchID  ,b.iBVid ,b.iPOsID 
+SELECT h.cVouchID ,h.cSource ,b.cSource ,b.PID,b.cCoVouchID  ,b.iBVid ,b.iPOsID ,b.cInvCode,b.iBVid,b.iPOsID,b.cCoVouchType
 FROM AP_ApplyPayVouchs b
 INNER JOIN AP_ApplyPayVouch h ON b.PID = h.PID 
 WHERE b.cCoVouchID = 'CGDD202401150061';
 
 SELECT t.cPOID ,t.POID  FROM PO_Pomain t WHERE t.cPOID = 'CGDD202401150061';
+
+付款申请单表头
+cSource 来源，cPerson 业务员，cDigest 备注  ，表头自定义项
+
+付款申请单表体
+cSource 来源，cCoVouchID 来源单据号  ，cInvCode  存货编码 ， iBVid 订单/发票子表ID  ，iPOsID 发票订单字表ID  ，表体自定义项
+
