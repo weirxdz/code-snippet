6ab296e68b65a8f3f68f1aaba499024a5886644b
ADMIN <151980581+weirxdz@users.noreply.github.com>
2024-06-07 15:53:03 +0800

    修改制单人取值
    修改制单人取值

--- 东为/TRIGGER CopyData_83To_saleorderdet_销售合同数据传底表的触发器.sql
+++ 东为/TRIGGER CopyData_83To_saleorderdet_销售合同数据传底表的触发器.sql
@@ -1,20 +1,14 @@
-USE [ecology]
-GO
-/****** Object:  Trigger [dbo].[CopyData_83To_saleorderdetails]    Script Date: 2023-12-21 15:01:22 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+USE ecology;
 ALTER TRIGGER [dbo].[CopyData_83To_saleorderdetails]
 ON [dbo].[formtable_main_83]
-after UPDATE
+AFTER UPDATE
 AS
 BEGIN
    -- 声明变量
    DECLARE @oamainid INT;
    DECLARE @COUNT INT;
    DECLARE @COUNT_cpqd INT;
-  IF UPDATE(shwc)
+  IF UPDATE(shwc) 
     BEGIN
       set @oamainid = (select COALESCE(A.[id], 0) from [dbo].[formtable_main_83] A JOIN inserted I ON A.[id] = I.[id] where A.[sfdxht] = 1);
       set @COUNT = (select COUNT(A.[zbid]) from [dbo].[uf_saleorderdetails] A JOIN inserted I ON A.[zbid] = I.[id]);
@@ -30,7 +24,7 @@ BEGIN
       IF @oamainid <> 0 AND @COUNT = 0 and @COUNT_cpqd >=1
         BEGIN
           INSERT INTO uf_saleorderdetails ([zbid] ,[requestId] ,[sqr] ,[sqrq] ,[sqbm] ,[sqgs] ,[htlx] ,[headsl] ,[htbh] ,[htmc] ,[qykh] ,[khmc] ,[u8khbmbm] ,[u8khywybm] ,[nbkh]  ,[xmbm] ,[xmmc] ,[htqdrq] ,[htqddd] ,[htksrq] ,[htjsrq] ,[jhrq] ,[mpxq],[sfzsht] ,[sfkp] ,[sfwl] , [sfcdyf] , [u8tbzt] , [dr] , cMaker, cSTCode, cBusType, iVTid, xmclass, shdz)
-          SELECT A.[id] as [id] ,A.[requestId] as [requestId] ,A.[sqr] as [sqr] ,A.[sqrq] as [sqrq] ,A.[sqbm] as [sqbm] ,A.[sqgs] as [sqgs] ,A.[htlx] as [htlx] ,A.[headsl] as [headsl] ,A.[htbh] as [htbh] ,A.[htmc] as [htmc] ,A.[qykh] as [qykh] ,A.[khmc] as [khmc] ,A.[u8bm] as [u8khbmbm] ,A.[u8ywy] as [u8khywybm] ,x.name as [nbkh] ,A.[xmbm] as [xmbm] ,A.[xmmc] as [xmmc] ,A.[htqdrq] ,A.[htqddd] ,A.[htksrq] ,A.[htjsrq] ,A.[jhrq] ,A.[mpxq] as [mpxq],A.[sfzsht] as [sfzsht] ,A.[sfkp] as [sfkp] ,case when A.[sfwl] = 1 then '否' when A.[sfwl] = 0 then '是' end as [sfwl] ,case when A.[sfwfcdyf] = 1 then '否' when A.[sfwfcdyf] = 0 then '是' end  as [sfcdyf] ,0 as [u8tbzt] ,0 as [dr] ,isnull(a.cmaker,'demo') as cMaker,A.xsddlx as cSTCode,ST.cSTName AS cBusType,95 as iVTid,'00' as xmclass,A.shdz AS shdz 
+          SELECT A.[id] as [id] ,A.[requestId] as [requestId] ,A.[sqr] as [sqr] ,A.[sqrq] as [sqrq] ,A.[sqbm] as [sqbm] ,A.[sqgs] as [sqgs] ,A.[htlx] as [htlx] ,A.[headsl] as [headsl] ,A.[htbh] as [htbh] ,A.[htmc] as [htmc] ,A.[qykh] as [qykh] ,A.[khmc] as [khmc] ,A.[u8bm] as [u8khbmbm] ,A.[u8ywy] as [u8khywybm] ,x.name as [nbkh] ,A.[xmbm] as [xmbm] ,A.[xmmc] as [xmmc] ,A.[htqdrq] ,A.[htqddd] ,A.[htksrq] ,A.[htjsrq] ,A.[jhrq] ,A.[mpxq] as [mpxq],A.[sfzsht] as [sfzsht] ,A.[sfkp] as [sfkp] ,case when A.[sfwl] = 1 then '否' when A.[sfwl] = 0 then '是' end as [sfwl] ,case when A.[sfwfcdyf] = 1 then '否' when A.[sfwfcdyf] = 0 then '是' end  as [sfcdyf] ,0 as [u8tbzt] ,0 as [dr] ,case when A.cmaker IS null or A.cmaker = '' then ' demo'else A.cmaker end as cMaker,A.xsddlx as cSTCode,ST.cSTName AS cBusType,95 as iVTid,'00' as xmclass,A.shdz AS shdz 
           FROM [dbo].[formtable_main_83] A
 		  left join mode_selectitempagedetail x on x.mainid = 27 and x.disorder = A.nbkh
 		  LEFT JOIN UFDATA_001_2023.dbo.SaleType st ON A.xsddlx = ST.cSTCode 
@@ -226,3 +220,4 @@ BEGIN
         END
     END   
 END;
+USE master;
